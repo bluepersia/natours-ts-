0322d0007575d79b669f0ca5c59c390359098b94
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-01-28 10:12:46 -0600

    CRUD 404

--- src/controllers/factory.ts
+++ src/controllers/factory.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { Model } from "mongoose";
 import APIFeatures from "../utility/APIFeatures";
+import AppError from "../utility/AppError";
 const handle = require ('express-async-handler')
 
 export const getAll = (Model:Model<any>) => handle (async (req:Request, res:Response) : Promise<void> => 
@@ -34,6 +35,9 @@ export const getOne = (Model:Model<any>) => handle (async (req:Request, res:Resp
 {
     const doc = await Model.findById (req.params.id);
 
+    if (!doc)
+        throw new AppError ('No document was found with that ID', 404);
+
     res.status (200).json ({
         status: 'success',
         data: {
@@ -46,6 +50,9 @@ export const updateOne = (Model:Model<any>) => handle (async (req:Request, res:R
 {
     const doc = await Model.findByIdAndUpdate (req.params.id, req.body, {new:true, runValidators:true});
 
+    if (!doc)
+        throw new AppError ('No document was found with that ID', 404);
+
     res.status (200).json ({
         status: 'success',
         data: {
@@ -57,6 +64,9 @@ export const updateOne = (Model:Model<any>) => handle (async (req:Request, res:R
 export const deleteOne = (Model:Model<any>) => handle (async (req:Request, res:Response) : Promise<void> =>
 {
     const doc = await Model.findByIdAndDelete (req.params.id);
+
+    if (!doc)
+        throw new AppError ('No document was found with that ID', 404);
 
     res.status (204).json ({
         status: 'success',
