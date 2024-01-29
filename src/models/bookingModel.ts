4985004997349755a5a9801a01beec19e6735634
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-01-29 11:30:48 -0600

    Booking Model

---
+++ src/models/bookingModel.ts
@@ -0,0 +1,44 @@
+import {HydratedDocument, Schema, model} from 'mongoose';
+import { ITour } from './tourModel';
+import { IUser } from './userModel';
+
+
+export interface IBooking 
+{
+    tour: HydratedDocument<ITour>,
+    user: HydratedDocument<IUser>,
+    price: number,
+    createdAt: Date,
+    paid: boolean
+}
+
+const bookingSchema = new Schema<IBooking> ({
+    tour: {
+        type: Schema.ObjectId,
+        ref: 'Tour',
+        required: true
+    },
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type:Date,
+        default: Date.now()
+    },
+    paid: {
+        type: Boolean,
+        default: true
+    }
+})
+
+
+const Booking = model ('Booking', bookingSchema);
+
+
+export default Booking;
